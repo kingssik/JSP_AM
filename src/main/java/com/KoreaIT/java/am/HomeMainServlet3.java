9581c7c5c38cb4b4b97a5e45dd3e4edc15ccc9ae
kingssik <hyunsikkang25@gamil.com>
2022-09-13 16:38:48 +0900

    구구단, 파라미터 기초

--- src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
@@ -9,15 +9,28 @@ import java.io.IOException;
 
 @WebServlet("/home/printDan")
 public class HomeMainServlet3 extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		response.setContentType("text/html; charset=UTF-8");
+
+		String inputedDan = request.getParameter("dan");
+
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
 		
-		response.getWriter().append("9단<br>");
-		
-		int dan = 9;
-		for(int i = 1; i <= 9; i++) {
+		String inputedLimit = request.getParameter("limit");
+
+		if (inputedLimit == null) {
+			inputedLimit = "9";
+		}
+
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("%d단<br />", dan));
+		for (int i = 1; i <= limit; i++) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
 		}
 	}
